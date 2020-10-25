67bfa916eebd55b989610146aafab2588139c06e
Rohitdutt <parsairohit2@gmail.com>
2020-10-25 19:13:05 +0530

    changes in CustomerController

--- customer/src/main/java/customer/controller/CustomerController.java
+++ customer/src/main/java/customer/controller/CustomerController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//Controller class for customer service
 public class CustomerController {
 
     @GetMapping("/customer")
