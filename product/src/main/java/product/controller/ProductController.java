8ae5ed3654a35ca6081ecefe842fb753353942cf
Rohitdutt <parsairohit2@gmail.com>
2020-10-25 19:13:34 +0530

    changes in ProductController

--- product/src/main/java/product/controller/ProductController.java
+++ product/src/main/java/product/controller/ProductController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//Controller class for product service
 public class ProductController {
 
     @GetMapping("/product")
