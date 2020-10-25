b6b24da2ecf90335f8e330eeebe83fb8eb4960c2
Rohitdutt <parsairohit2@gmail.com>
2020-10-25 19:13:22 +0530

    changes in OrderController

--- order/src/main/java/order/controller/OrderController.java
+++ order/src/main/java/order/controller/OrderController.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//controller class for order service
 public class OrderController {
-
     @GetMapping("/order")
     public String hello(){
         return "hello world from order";
